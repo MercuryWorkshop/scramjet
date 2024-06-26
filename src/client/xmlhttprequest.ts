3b277923253ab7af82ee15d18f5e56aec3a75716
Avad3 <65318266+Avad3@users.noreply.github.com>
2024-06-26 14:28:00 -0400

    add xmlhttprequest support

---
+++ src/client/xmlhttprequest.ts
@@ -0,0 +1,18 @@
+XMLHttpRequest.prototype.open = new Proxy(XMLHttpRequest.prototype.open, {
+  apply(target, thisArg, argArray) {
+    if (argArray[1]) argArray[1] = self.__scramjet$bundle.rewriters.url.encodeUrl(argArray[1]);
+
+    return Reflect.apply(target, thisArg, argArray);
+  },
+});
+
+XMLHttpRequest.prototype.setRequestHeader = new Proxy(XMLHttpRequest.prototype.setRequestHeader, {
+  apply(target, thisArg, argArray) {
+    let headerObject = Object.fromEntries(argArray);
+    headerObject = self.__scramjet$bundle.rewriters.rewriteHeaders(headerObject);
+
+    argArray = Object.entries(headerObject);
+
+    return Reflect.apply(target, thisArg, argArray);
+  },
+});
