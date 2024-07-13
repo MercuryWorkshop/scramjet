19a27f6c634844ec839ae1090f5a87cad0fd055c
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-12 23:59:14 -0500

    clean up sw registration

--- static/sw.js
+++ static/sw.js
@@ -4,12 +4,14 @@ import "./scramjet.config.js";
 
 const scramjet = new ScramjetServiceWorker();
 
-self.addEventListener("fetch", async (event) => {
-    event.respondWith((async() => {
-        if (scramjet.route(event)) {
-            return await scramjet.fetch(event);
-        } else {
-            return await fetch(event.request);
-        }
-    })())
+async function handleRequest(event) {
+    if (scramjet.route(event)) {
+        return scramjet.fetch(event);
+    }
+
+    return fetch(event.request)
+}
+
+self.addEventListener("fetch", (event) => {
+    event.respondWith(handleRequest(event));
 });
