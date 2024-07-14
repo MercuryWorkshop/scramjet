38f7a5e5a71cbf5afb510cf4486cc1ef3d90163d
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-13 21:21:30 -0500

    clean up location proxy

--- src/client/location.ts
+++ src/client/location.ts
@@ -1,18 +1,12 @@
 // @ts-nocheck
 import { encodeUrl, decodeUrl } from "../shared";
 
-function urlLocation() {
+export function LocationProxy() {
     const loc = new URL(decodeUrl(location.href));
     loc.assign = (url: string) => location.assign(encodeUrl(url));
     loc.reload = () => location.reload();
     loc.replace = (url: string) => location.replace(encodeUrl(url));
     loc.toString = () => loc.href;
-        
-    return loc;
-}
-
-export function LocationProxy() {
-    const loc = urlLocation();
 
     return new Proxy(window.location, {
         get(target, prop) {
