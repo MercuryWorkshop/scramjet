780dc005f3ce432b8e2e48270ffbdf54fc845eed
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-15 14:27:29 -0500

    fix history client api

--- src/client/history.ts
+++ src/client/history.ts
@@ -2,7 +2,7 @@ import { decodeUrl } from "./shared";
 
 window.history.pushState = new Proxy(window.history.pushState, {
 	apply(target, thisArg, argArray) {
-		argArray[3] = decodeUrl(argArray[3]);
+		argArray[2] = decodeUrl(argArray[2]);
 
 		return Reflect.apply(target, thisArg, argArray);
 	},
@@ -10,7 +10,7 @@ window.history.pushState = new Proxy(window.history.pushState, {
 
 window.history.replaceState = new Proxy(window.history.replaceState, {
 	apply(target, thisArg, argArray) {
-		argArray[3] = decodeUrl(argArray[3]);
+		argArray[2] = decodeUrl(argArray[2]);
 
 		return Reflect.apply(target, thisArg, argArray);
 	},
