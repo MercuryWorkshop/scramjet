fa779017ad0386fcfe74e4f7f27dc8db38e2dde3
Percs <83934299+Percslol@users.noreply.github.com>
2024-07-16 17:39:55 -0500

    fix worker rewriter

--- src/shared/rewriters/worker.ts
+++ src/shared/rewriters/worker.ts
@@ -1,9 +1,8 @@
 import { rewriteJs } from "./js";
 export function rewriteWorkers(js: string, origin?: URL) {
-	let str = new String().toString()[
-		//@ts-expect-error
-		("codecs", "config", "shared", "client")
-	].forEach((script) => {
+	let str = new String().toString();
+	
+	["codecs", "config", "shared", "client"].forEach((script) => {
 		str += `import "${self.$scramjet.config[script]}"\n`;
 	});
 	str += rewriteJs(js, origin);
