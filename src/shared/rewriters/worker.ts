817f6876217547bc0661a6f24771e2bcb04cb5f4
velzie <velzie@velzie.rip>
2024-07-18 21:34:36 -0400

    don't rewrite workers

--- src/shared/rewriters/worker.ts
+++ src/shared/rewriters/worker.ts
@@ -1,11 +1,11 @@
 import { rewriteJs } from "./js";
 export function rewriteWorkers(js: string, origin?: URL) {
 	let str = new String().toString();
+	//
+	// ["codecs", "config", "shared", "client"].forEach((script) => {
+	// 	str += `import "${self.$scramjet.config[script]}"\n`;
+	// });
+	// str += rewriteJs(js, origin);
 
-	["codecs", "config", "shared", "client"].forEach((script) => {
-		str += `import "${self.$scramjet.config[script]}"\n`;
-	});
-	str += rewriteJs(js, origin);
-
-	return str;
+	return js;
 }
