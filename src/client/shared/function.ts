ecbdb4177c0b45c41de1e901a088f61b9b18dedd
Avad3 <65318266+Avad3@users.noreply.github.com>
2024-07-28 21:48:49 -0400

    rename eval.ts to function.ts

---
+++ src/client/shared/function.ts
@@ -0,0 +1,22 @@
+import { ScramjetClient, ProxyCtx } from "../client";
+import { rewriteJs } from "../shared";
+
+function rewriteFunction(ctx: ProxyCtx) {
+    const stringifiedFunction = ctx.fn(...ctx.args).toString();
+
+    ctx.return(ctx.fn(`return ${rewriteJs(stringifiedFunction)}`)());
+}
+
+export default function (client: ScramjetClient, self: Self) {
+    client.Proxy("Function", {
+        apply(ctx) {
+            rewriteFunction(ctx);
+        },
+
+        construct(ctx) {
+            rewriteFunction(ctx);
+        },
+    });
+
+    Function.prototype.constructor = Function;
+}
