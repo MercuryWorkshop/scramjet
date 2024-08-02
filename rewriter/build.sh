4549f3e672ad649037d9a31e69b2f7bf2088af8a
Toshit Chawda <r58playz@gmail.com>
2024-08-02 15:20:54 -0700

    optimizations

--- rewriter/build.sh
+++ rewriter/build.sh
@@ -1,4 +1,8 @@
-RUSTFLAGS='-C target-feature=+atomics,+bulk-memory -Zlocation-detail=none' cargo build --lib --target wasm32-unknown-unknown -Z build-std=panic_abort,std -Z build-std-features=panic_immediate_abort --features "$FEATURES" --release
+#!/usr/bin/env bash
+set -euo pipefail
+shopt -s inherit_errexit
+
+RUSTFLAGS='-C target-feature=+atomics,+bulk-memory -Zlocation-detail=none' cargo build --lib --target wasm32-unknown-unknown -Z build-std=panic_abort,std -Z build-std-features=panic_immediate_abort --features "${FEATURES:-}" --release
 wasm-bindgen --weak-refs --target web --out-dir out/ target/wasm32-unknown-unknown/release/rewriter.wasm
 
 sed -i 's/import.meta.url/""/g' out/rewriter.js
@@ -7,8 +11,13 @@ cd ..
 
 WASM=rewriter/out/rewriter_bg.wasm
 
-time wasm-opt -g -O4 --vacuum --dce --enable-threads --enable-bulk-memory --enable-simd "$WASM" -o rewriter/out/optimized.wasm
-# cp "$WASM" rewriter/out/optimized.wasm
+if ! [ "${RELEASE:-0}" = "1" ]; then
+	WASMOPTFLAGS="-g"
+else
+	WASMOPTFLAGS=""
+fi
+
+time wasm-opt $WASMOPTFLAGS -O4 --vacuum --dce --enable-threads --enable-bulk-memory --enable-simd "$WASM" -o rewriter/out/optimized.wasm
 
 echo -n "self.WASM = '" > dist/scramjet.wasm.js
 base64 -w0 < "rewriter/out/optimized.wasm" >> dist/scramjet.wasm.js
