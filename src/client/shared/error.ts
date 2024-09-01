b47f19ac60da24420a0bc81ffffc172fe13a158a
velzie <velzie@velzie.rip>
2024-09-01 19:52:39 -0400

    error.stack

---
+++ src/client/shared/error.ts
@@ -0,0 +1,18 @@
+import { decodeUrl } from "../../shared";
+import { ScramjetClient } from "../client";
+
+export default function (client: ScramjetClient, self: Self) {
+	// v8 only. all we need to do is clean the scramjet urls from stack traces
+	Error.prepareStackTrace = (error, stack) => {
+		let newstack = error.stack;
+
+		for (let i = 0; i < stack.length; i++) {
+			const url = stack[i].getFileName();
+			try {
+				newstack = newstack.replaceAll(url, decodeUrl(url));
+			} catch {}
+		}
+
+		return newstack;
+	};
+}
