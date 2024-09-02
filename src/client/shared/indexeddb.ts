6bac2dde2faf18627d9fbda971ac6fc5695981a1
velzie <velzie@velzie.rip>
2024-09-01 20:35:09 -0400

    indexeddb

---
+++ src/client/shared/indexeddb.ts
@@ -0,0 +1,16 @@
+import { ScramjetClient } from "../client";
+
+export default function (client: ScramjetClient, self: Self) {
+	client.Proxy("IDBFactory.prototype.open", {
+		apply(ctx) {
+			ctx.args[0] = `${client.url.origin}@${ctx.args[0]}`;
+		},
+	});
+
+	client.Trap("IDBDatabase.prototype.name", {
+		get(ctx) {
+			let name = ctx.get() as string;
+			return name.substring(name.indexOf("@") + 1);
+		},
+	});
+}
