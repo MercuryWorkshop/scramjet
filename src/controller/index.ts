293aeb2956e767956f4194dccbc764de58d81bc2
Percs <83934299+Percslol@users.noreply.github.com>
2024-10-03 21:32:33 -0500

    make flags not have to all be specified

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -37,7 +37,17 @@ export class ScramjetController {
 			},
 		};
 
-		this.config = Object.assign({}, defaultConfig, config);
+		const deepMerge = (target: any, source: any): any => {
+			for (const key in source) {
+				if (source[key] instanceof Object && key in target) {
+					Object.assign(source[key], deepMerge(target[key], source[key]));
+				}
+			}
+
+			return Object.assign(target || {}, source);
+		};
+
+		this.config = deepMerge(defaultConfig, config);
 	}
 
 	async init(serviceWorkerPath: string): Promise<ServiceWorkerRegistration> {
