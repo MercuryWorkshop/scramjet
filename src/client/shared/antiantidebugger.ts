797607aabb519495d5298b38bdb82b4a4375d685
velzie <velzie@velzie.rip>
2024-10-12 10:58:33 -0400

    for some reason we have to delete revokeObjectURL???

---
+++ src/client/shared/antiantidebugger.ts
@@ -0,0 +1,26 @@
+import { ScramjetClient } from "../client";
+
+export default function (client: ScramjetClient) {
+	client.Proxy("console.clear", {
+		apply(ctx) {
+			// fuck you
+			ctx.return(undefined);
+		},
+	});
+
+	const log = console.log;
+	client.Trap("console.log", {
+		set(ctx, v) {
+			// is there a legitimate reason to let sites do this?
+		},
+		get(ctx) {
+			return log;
+		},
+	});
+
+	client.Proxy("URL.revokeObjectURL", {
+		apply(ctx) {
+			ctx.return(undefined);
+		},
+	});
+}
