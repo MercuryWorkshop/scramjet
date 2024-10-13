0906dd78a9d50cb20c85fa77c278a23e48ec11d8
velzie <velzie@velzie.rip>
2024-10-13 14:16:36 -0400

    don't create a worker that doesnt fucking exist

--- src/client/shared/requests/xmlhttprequest.ts
+++ src/client/shared/requests/xmlhttprequest.ts
@@ -11,7 +11,7 @@ if (self.Worker) {
 export default function (client: ScramjetClient, self: Self) {
 	let worker;
 	if (self.Worker) {
-		worker = new nativeworker(config.sync);
+		worker = new nativeworker(config.files.sync);
 	}
 	const ARGS = Symbol("xhr original args");
 	const HEADERS = Symbol("xhr headers");
