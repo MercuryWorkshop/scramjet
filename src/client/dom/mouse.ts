0503b3a408652a9bc05d5ff3f207a8deaa4fa621
Percs <83934299+Percslol@users.noreply.github.com>
2024-10-18 02:45:52 -0500

    chore: prettier

--- src/client/dom/mouse.ts
+++ src/client/dom/mouse.ts
@@ -1,12 +1,12 @@
 import { ScramjetClient } from "../client";
 
 export default function (client: ScramjetClient, self: typeof window) {
-    client.Proxy("MouseEvent.prototype.initMouseEvent", {
-        apply(ctx) {
-            // Proxy(Window) instanceof Window SHOULD return true. But uhh, it doesn't.
-            ctx.args[3] = self;
-            
-            ctx.call();
-        },
-    })
+	client.Proxy("MouseEvent.prototype.initMouseEvent", {
+		apply(ctx) {
+			// Proxy(Window) instanceof Window SHOULD return true. But uhh, it doesn't.
+			ctx.args[3] = self;
+
+			ctx.call();
+		},
+	});
 }
