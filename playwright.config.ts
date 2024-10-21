5d809faa75e1c32c0f6cb51a13e91cbb51866fb3
Percs <83934299+Percslol@users.noreply.github.com>
2024-10-21 13:44:55 -0500

    chore: prettier

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,36 +4,36 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
-  fullyParallel: true,
-  forbidOnly: !!process.env.CI,
-  retries: 0,
-  reporter: "html",
-  timeout: 20000,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-    actionTimeout: 5000,
-    baseURL: "http://localhost:1337",
-  },
+	testDir: "./tests",
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI,
+	retries: 0,
+	reporter: "html",
+	timeout: 20000,
+	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+	use: {
+		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+		trace: "on-first-retry",
+		actionTimeout: 5000,
+		baseURL: "http://localhost:1337",
+	},
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-  ],
+	/* Configure projects for major browsers */
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+		// {
+		//   name: "firefox",
+		//   use: { ...devices["Desktop Firefox"] },
+		// },
+	],
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "pnpm run dev",
-    url: "http://127.0.0.1:1337",
-    reuseExistingServer: false,
-  },
+	/* Run your local dev server before starting the tests */
+	webServer: {
+		command: "pnpm run dev",
+		url: "http://127.0.0.1:1337",
+		reuseExistingServer: false,
+	},
 });
