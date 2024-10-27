b9be6e8c7ad2f5e2eb8daef0b87442cb8e045370
Percs <83934299+Percslol@users.noreply.github.com>
2024-10-26 22:56:07 -0500

    feat: switch to new css rewriter

--- src/shared/rewriters/css.ts
+++ src/shared/rewriters/css.ts
@@ -1,64 +1,42 @@
-// This CSS rewriter uses code from Meteor
-// You can find the original source code at https://github.com/MeteorProxy/Meteor
-
 import { URLMeta, rewriteUrl, unrewriteUrl } from "./url";
 
 export function rewriteCss(css: string, meta: URLMeta) {
-	const regex =
-		/(@import\s+(?!url\())?\s*url\(\s*(['"]?)([^'")]+)\2\s*\)|@import\s+(['"])([^'"]+)\4/g;
-
-	return css.replace(
-		regex,
-		(
-			match,
-			importStatement,
-			urlQuote,
-			urlContent,
-			importQuote,
-			importContent
-		) => {
-			const url = urlContent || importContent;
-			const encodedUrl = rewriteUrl(url.trim(), meta);
-
-			if (importStatement) {
-				return `@import url(${urlQuote}${encodedUrl}${urlQuote})`;
-			}
-
-			if (importQuote) {
-				return `@import ${importQuote}${encodedUrl}${importQuote}`;
-			}
-
-			return `url(${urlQuote}${encodedUrl}${urlQuote})`;
-		}
-	);
+	handleCss("unrewrite", css, meta);
 }
 
 export function unrewriteCss(css: string) {
-	const regex =
-		/(@import\s+(?!url\())?\s*url\(\s*(['"]?)([^'")]+)\2\s*\)|@import\s+(['"])([^'"]+)\4/g;
+	handleCss("rewrite", css);
+}
 
-	return css.replace(
-		regex,
-		(
-			match,
-			importStatement,
-			urlQuote,
-			urlContent,
-			importQuote,
-			importContent
-		) => {
-			const url = urlContent || importContent;
-			const encodedUrl = unrewriteUrl(url.trim());
+function handleCss(type: "rewrite" | "unrewrite", css: string, meta?: URLMeta) {
+	const urlRegex = /url\(['"]?(.+?)['"]?\)/gm;
+	const Atruleregex =
+		/@import\s+(url\s*?\(.{0,9999}?\)|['"].{0,9999}?['"]|.{0,9999}?)($|\s|;)/gm;
+	css = new String(css).toString();
+	css = css.replace(urlRegex, (match, url) => {
+		const encodedUrl =
+			type === "rewrite"
+				? rewriteUrl(url.trim(), meta)
+				: unrewriteUrl(url.trim());
 
-			if (importStatement) {
-				return `@import url(${urlQuote}${encodedUrl}${urlQuote})`;
+		return match.replace(url, encodedUrl);
+	});
+	css = css.replace(Atruleregex, (_, importStatement) => {
+		return importStatement.replace(
+			/^(url\(['"]?|['"]|)(.+?)(['"]|['"]?\)|)$/gm,
+			(match, firstQuote, url, endQuote) => {
+				if (firstQuote.startsWith("url")) {
+					return match;
+				}
+				const encodedUrl =
+					type === "rewrite"
+						? rewriteUrl(url.trim(), meta)
+						: unrewriteUrl(url.trim());
+
+				return `${firstQuote}${encodedUrl}${endQuote}`;
 			}
+		);
+	});
 
-			if (importQuote) {
-				return `@import ${importQuote}${encodedUrl}${importQuote}`;
-			}
-
-			return `url(${urlQuote}${encodedUrl}${urlQuote})`;
-		}
-	);
+	return css;
 }
