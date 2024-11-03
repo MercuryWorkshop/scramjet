d632041355deb9f6ad3c2ea8915dce46a5f40ff6
Percs <83934299+Percslol@users.noreply.github.com>
2024-11-02 22:43:56 -0500

    fix: history proxy

--- src/client/dom/history.ts
+++ src/client/dom/history.ts
@@ -5,7 +5,8 @@ import { UrlChangeEvent } from "../events";
 export default function (client: ScramjetClient, self: typeof globalThis) {
 	client.Proxy("History.prototype.pushState", {
 		apply(ctx) {
-			if (ctx.args[2]) ctx.args[2] = rewriteUrl(ctx.args[2], client.meta);
+			if (ctx.args[2] || ctx.args[2] === "")
+				ctx.args[2] = rewriteUrl(ctx.args[2], client.meta);
 			ctx.call();
 
 			const ev = new UrlChangeEvent(client.url.href);
@@ -15,7 +16,8 @@ export default function (client: ScramjetClient, self: typeof globalThis) {
 
 	client.Proxy("History.prototype.replaceState", {
 		apply(ctx) {
-			if (ctx.args[2]) ctx.args[2] = rewriteUrl(ctx.args[2], client.meta);
+			if (ctx.args[2] || ctx.args[2] === "")
+				ctx.args[2] = rewriteUrl(ctx.args[2], client.meta);
 			ctx.call();
 
 			const ev = new UrlChangeEvent(client.url.href);
