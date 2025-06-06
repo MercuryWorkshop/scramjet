031c0190c26e71e6f3001ffa6a3946dd47b328f7
Toshit Chawda <r58playz@gmail.com>
2025-06-06 11:44:27 -0700

    add clap

--- rewriter/native/src/main.rs
+++ rewriter/native/src/main.rs
@@ -1,73 +1,86 @@
 use std::{
 	env, fs,
-	str::FromStr,
+	path::PathBuf,
 	sync::Arc,
 	time::{Duration, Instant},
 };
 
 use anyhow::{Context, Result};
+use clap::Parser;
 use oxc::diagnostics::NamedSource;
 use rewriter::NativeRewriter;
 
 mod rewriter;
 mod test_runner;
 
+#[derive(Parser)]
+#[command(version = clap::crate_version!())]
+pub enum Cli {
+	/// Rewrite a file
+	Rewrite { file: PathBuf },
+	/// Rewrite a file multiple times for flamegraphing
+	Bench { file: PathBuf, iterations: u32 },
+}
+
 fn main() -> Result<()> {
-	let file = env::args().nth(1).unwrap_or_else(|| "test.js".to_string());
-	let data = fs::read_to_string(file).context("failed to read file")?;
-	let bench = env::args().nth(2).map(|x| usize::from_str(&x));
+	let args = Cli::parse();
 
 	let mut rewriter = NativeRewriter::new().context("failed to make rewriter")?;
 
-	if let Some(cnt) = bench.transpose().context("invalid bench size")? {
-		let mut duration = Duration::from_secs(0);
+	match args {
+		Cli::Rewrite { file } => {
+			let data = fs::read_to_string(file).context("failed to read file")?;
 
-		let cnt = cnt * 100;
+			let res = rewriter.rewrite(&data)?;
 
-		for x in 1..=cnt {
-			let before = Instant::now();
-			rewriter.rewrite(&data).context("failed to rewrite")?;
-			let after = Instant::now();
+			let source = Arc::new(
+				NamedSource::new(data.clone(), "https://google.com/glorngle/si.js")
+					.with_language("javascript"),
+			);
+
+			println!(
+				"rewritten:\n{}",
+				str::from_utf8(&res.js).context("failed to parse rewritten js")?
+			);
+
+			let unrewritten = NativeRewriter::unrewrite(&res);
+
+			eprintln!("errors:");
+			for err in res.errors {
+				eprintln!("{}", err.with_source_code(source.clone()));
+			}
 
 			rewriter.reset();
 
-			duration += after - before;
+			println!(
+				"unrewritten matches orig: {}",
+				data.as_bytes() == unrewritten.as_slice()
+			);
+		}
+		Cli::Bench { file, iterations } => {
+			let data = fs::read_to_string(file).context("failed to read file")?;
+			let mut duration = Duration::from_secs(0);
 
-			if x % 100 == 0 {
-				println!("{x}...");
+			let cnt = iterations * 100;
+
+			for x in 1..=cnt {
+				let before = Instant::now();
+				rewriter.rewrite(&data).context("failed to rewrite")?;
+				let after = Instant::now();
+
+				rewriter.reset();
+
+				duration += after - before;
+
+				if x % 100 == 0 {
+					println!("{x}...");
+				}
 			}
+
+			println!("iterations: {cnt}");
+			println!("total time: {duration:?}");
+			println!("avg time: {:?}", duration / cnt);
 		}
-
-		println!("iterations: {cnt}");
-		println!("total time: {duration:?}");
-		println!("avg time: {:?}", duration / cnt as u32);
-	} else {
-		println!("orig:\n{data}");
-
-		let res = rewriter.rewrite(&data)?;
-
-		let source = Arc::new(
-			NamedSource::new(data.clone(), "https://google.com/glorngle/si.js")
-				.with_language("javascript"),
-		);
-		eprintln!("errors:");
-		for err in res.errors.clone() {
-			eprintln!("{}", err.with_source_code(source.clone()));
-		}
-
-		println!(
-			"rewritten:\n{}",
-			str::from_utf8(&res.js).context("failed to parse rewritten js")?
-		);
-
-		let unrewritten = NativeRewriter::unrewrite(&res);
-
-		rewriter.reset();
-
-		println!(
-			"unrewritten matches orig: {}",
-			data.as_bytes() == unrewritten.as_slice()
-		);
 	}
 
 	Ok(())
