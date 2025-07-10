5fc5127ee154895f41cbc288bfed99de51120a75
velzie <velzie@velzie.rip>
2025-07-10 10:45:45 -0400

    export typedefs for scramjet controller

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,3 +1,14 @@
 declare const scramjetPath: string;
 
+import * as controller from "../dist/types/controller/index.ts";
+import * as types from "../dist/types/types.ts";
+import * as frame from "../dist/types/controller/frame.ts";
+
+declare global {
+	const ScramjetController: typeof controller.ScramjetController;
+	const ScramjetFrame: typeof frame.ScramjetFrame;
+
+	type ScramjetConfig = types.ScramjetConfig;
+	type ScramjetInitConfig = types.ScramjetInitConfig;
+}
 export { scramjetPath };
