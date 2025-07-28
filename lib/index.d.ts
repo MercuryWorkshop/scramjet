f06044c97d1e9d3dc8a4fb4c1db7e0b542ff27c7
velzie <velzie@velzie.rip>
2025-07-28 10:45:42 -0400

    [core] fix a bad typedef import

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,7 +4,7 @@ import * as controller from "../dist/types/controller/index.ts";
 import * as worker from "../dist/types/worker/index.ts";
 import * as types from "../dist/types/types.ts";
 import * as frame from "../dist/types/controller/frame.ts";
-import { ScramjetClient as _ScramjetClient } from "../dist/types/client/client.ts";
+import { ScramjetClient as _ScramjetClient } from "../dist/types/client/index.ts";
 
 declare global {
 	function $scramjetLoadController(): typeof controller;
