1f2c521a5e067fdcb1ac3579f905302ae42797ba
velzie <velzie@velzie.rip>
2025-08-04 12:18:38 -0400

    [core] fix rewriter panic

--- src/worker/fetch.ts
+++ src/worker/fetch.ts
@@ -285,7 +285,7 @@ export async function handleFetch(
 				// @ts-ignore why the fuck is this not typed microsoft
 				duplex: "half",
 			}));
-		response.finalURL = ev.url;
+		response.finalURL = ev.url.href;
 
 		return await handleResponse(
 			url,
