79fad91a0aee81cc3839aea9cc8c5435d127fb39
velzie <velzie@velzie.rip>
2025-08-25 11:13:03 -0400

    [core] export ScramjetDownload type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,6 +4,7 @@ import * as controller from "../dist/types/controller/index.ts";
 import * as worker from "../dist/types/worker/index.ts";
 import * as types from "../dist/types/types.ts";
 import * as frame from "../dist/types/controller/frame.ts";
+import * as events from "../dist/types/client/events.ts";
 import { ScramjetClient as _ScramjetClient } from "../dist/types/client/index.ts";
 
 declare global {
@@ -13,6 +14,7 @@ declare global {
 	type ScramjetController = controller.ScramjetController;
 	type ScramjetFrame = frame.ScramjetFrame;
 	type ScramjetClient = _ScramjetClient;
+	type ScramjetDownload = events.ScramjetDownload;
 
 	type ScramjetConfig = types.ScramjetConfig;
 	type ScramjetInitConfig = types.ScramjetInitConfig;
