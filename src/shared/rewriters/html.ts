db994ea537138bd5d3ede2f4f68b940af7aac359
velzie <velzie@velzie.rip>
2025-08-28 13:02:09 -0400

    [core] allow loading scramjetclient without auto injecting

--- src/shared/rewriters/html.ts
+++ src/shared/rewriters/html.ts
@@ -46,7 +46,7 @@ function rewriteHtmlInner(
 		const dump = JSON.stringify(cookieStore.dump());
 		const injected = `
 			self.COOKIE = ${dump};
-			$scramjetLoadClient(${JSON.stringify(config)});
+			$scramjetLoadClient().loadAndHook(${JSON.stringify(config)})
 			if ("document" in self && document?.currentScript) {
 				document.currentScript.remove();
 			}
