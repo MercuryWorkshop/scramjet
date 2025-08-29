502ae5d8b88ada0e5627a7cc8b41eb2789189516
velzie <velzie@velzie.rip>
2025-08-29 12:05:39 -0400

    [core] flatten exports

--- src/shared/rewriters/index.ts
+++ src/shared/rewriters/index.ts
@@ -1,15 +1,7 @@
-export * as css from "./css";
-export * as headers from "./headers";
-export * as html from "./html";
-export * as js from "./js";
-export * as url from "./url";
-export * as worker from "./worker";
+export * from "./css";
+export * from "./headers";
+export * from "./html";
+export * from "./js";
+export * from "./url";
+export * from "./worker";
 export * from "./wasm";
-
-export type * from "./css";
-export type * from "./headers";
-export type * from "./html";
-export type * from "./js";
-export type * from "./url";
-export type * from "./worker";
-export type * from "./wasm";
