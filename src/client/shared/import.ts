0251b91776e712f450b6969c85bd16cb402728c6
velzie <velzie@velzie.rip>
2025-09-03 12:18:57 -0400

    [core] use Object.defineProperty for importfn and metafn

--- src/client/shared/import.ts
+++ src/client/shared/import.ts
@@ -5,31 +5,40 @@ import { rewriteUrl } from "@rewriters/url";
 export default function (client: ScramjetClient, self: Self) {
 	const Function = client.natives.store["Function"];
 
-	self[config.globals.importfn] = function (base: string, url: string) {
-		const resolved = new URL(url, base).href;
+	Object.defineProperty(self, config.globals.importfn, {
+		value: function (base: string, url: string) {
+			const resolved = new URL(url, base).href;
 
-		if (
-			url.includes(":") ||
-			url.startsWith("/") ||
-			url.startsWith(".") ||
-			url.startsWith("..")
-		) {
-			// this is a url
-			return Function(
-				`return import("${rewriteUrl(resolved, client.meta)}?type=module")`
-			)();
-		} else {
-			// this is a specifier handled by importmaps
-			return Function(`return import("${url}")`)();
-		}
-	};
+			if (
+				url.includes(":") ||
+				url.startsWith("/") ||
+				url.startsWith(".") ||
+				url.startsWith("..")
+			) {
+				// this is a url
+				return Function(
+					`return import("${rewriteUrl(resolved, client.meta)}?type=module")`
+				)();
+			} else {
+				// this is a specifier handled by importmaps
+				return Function(`return import("${url}")`)();
+			}
+		},
+		writable: false,
+		configurable: false,
+		enumerable: false,
+	});
+	Object.defineProperty(self, config.globals.metafn, {
+		value: function (metaobj: any, base: string) {
+			metaobj.url = base;
+			metaobj.resolve = function (url: string) {
+				return new URL(url, base).href;
+			};
 
-	self[config.globals.metafn] = function (metaobj: any, base: string) {
-		metaobj.url = base;
-		metaobj.resolve = function (url: string) {
-			return new URL(url, base).href;
-		};
-
-		return metaobj;
-	};
+			return metaobj;
+		},
+		writable: false,
+		configurable: false,
+		enumerable: false,
+	});
 }
