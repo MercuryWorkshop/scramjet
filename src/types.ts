ab528125411390045aef82e71f757f90802d7047
velzie <velzie@velzie.rip>
2025-09-03 09:40:14 -0400

    [core] refactor rewriteJs and add allowInvalidJs flag

--- src/types.ts
+++ src/types.ts
@@ -14,6 +14,7 @@ export type ScramjetFlags = {
 	sourcemaps: boolean;
 	destructureRewrites: boolean;
 	interceptDownloads: boolean;
+	allowInvalidJs: boolean;
 };
 
 export interface ScramjetConfig {
