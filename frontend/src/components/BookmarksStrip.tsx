3fbd5aebfc8b725daed5797532a4b42c104c70ac
velzie <velzie@velzie.rip>
2025-09-18 18:18:54 -0400

    [frontend] add icons to bookmarks context menu

--- frontend/src/components/BookmarksStrip.tsx
+++ frontend/src/components/BookmarksStrip.tsx
@@ -1,6 +1,10 @@
 import { css, type Component } from "dreamland/core";
 import { Icon } from "./Icon";
 import iconAdd from "@ktibow/iconset-ion/add";
+import iconOpen from "@ktibow/iconset-ion/open-outline";
+import iconLink from "@ktibow/iconset-ion/link-outline";
+import iconBrush from "@ktibow/iconset-ion/brush-outline";
+import iconTrash from "@ktibow/iconset-ion/trash-outline";
 import { browser } from "../Browser";
 import { createMenu, setContextMenu } from "./Menu";
 
@@ -35,17 +39,21 @@ export const BookmarksStrip: Component = function (cx) {
 						createMenu(e.clientX, e.clientY, [
 							{
 								label: "Open",
+								icon: iconLink,
 								action: () => browser.activetab.pushNavigate(new URL(b.url)),
 							},
 							{
 								label: "Open in New Tab",
+								icon: iconOpen,
 								action: () => browser.newTab(new URL(b.url)),
 							},
 							{
 								label: "Edit Bookmark",
+								icon: iconBrush,
 							},
 							{
 								label: "Delete Bookmark",
+								icon: iconTrash,
 								action: () => {
 									browser.bookmarks = browser.bookmarks.filter((br) => br != b);
 								},
