aa0d5031d8c77e38b6693e90014c4288dd292b57
velzie <velzie@velzie.rip>
2025-09-26 17:05:26 -0400

    [core] export bare-mux-custom and ScramjetFetchResponse to scramjet/bundled

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+// NOTE: this is the entrypoint for scramjet.bundle.js
+// as such it exports everything in scramjet
+// the entry point for scramjet.all.js (what most sites wil use) is entry.ts
+
 import "./global.d";
 export * from "./client";
 export * from "./controller";
@@ -6,3 +10,5 @@ export * from "./worker";
 export * from "./entry";
 export * from "./symbols";
 export * from "./types";
+
+export { BareClient } from "./bare-mux-custom";
--- src/worker/fetch.ts
+++ src/worker/fetch.ts
@@ -54,7 +54,7 @@ export interface ScramjetFetchParsed {
 	scriptType: string;
 }
 
-interface ScramjetFetchResponse {
+export interface ScramjetFetchResponse {
 	body: BodyType;
 	headers: BareHeaders;
 	status: number;
