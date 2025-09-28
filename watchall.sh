0dfa330dd3e9a45747c0facd1dc1cfdbb7297db2
velzie <velzie@velzie.rip>
2025-09-28 11:23:47 -0400

    add back format script and move git hook writer to rspack.config.ts

--- watchall.sh
+++
@@ -1,7 +0,0 @@
-(cd playwright && npm run watch) &
-(cd playwright/web_builder && npx rollup -cw) &
-(pnpm dev) &
-(cd dreamlandjs && pnpm watch) &
-(cd chobitsu && pnpm webpack --watch --mode=development) &
-(cd page_inject && npx rollup -cw) &
-(cd frontend && pnpm dev) &
