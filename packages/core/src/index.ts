c5268ccce756f12fdee6106f109f588841a4ba79
velzie <velzie@velzie.rip>
2025-11-05 18:13:47 -0500

    [scramjet/core] export a default config

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -4,6 +4,7 @@
 
 import { setWasm } from "@rewriters/wasm";
 import "./global.d";
+import { ScramjetConfig } from "./types";
 export * from "./client";
 export * from "./shared";
 export * from "./symbols";
@@ -11,6 +12,38 @@ export * from "./types";
 export * from "./fetch";
 
 declare const REWRITERWASM: string | undefined;
+
+export const defaultConfig: ScramjetConfig = {
+	globals: {
+		wrapfn: "$scramjet$wrap",
+		wrappropertybase: "$scramjet__",
+		wrappropertyfn: "$scramjet$prop",
+		cleanrestfn: "$scramjet$clean",
+		importfn: "$scramjet$import",
+		rewritefn: "$scramjet$rewrite",
+		metafn: "$scramjet$meta",
+		wrappostmessagefn: "$scramjet$wrappostmessage",
+		pushsourcemapfn: "$scramjet$pushsourcemap",
+		trysetfn: "$scramjet$tryset",
+		templocid: "$scramjet$temploc",
+		tempunusedid: "$scramjet$tempunused",
+	},
+	flags: {
+		syncxhr: false,
+		strictRewrites: true,
+		rewriterLogs: false,
+		captureErrors: true,
+		cleanErrors: false,
+		scramitize: false,
+		sourcemaps: true,
+		destructureRewrites: false,
+		allowInvalidJs: false,
+		allowFailedIntercepts: false,
+	},
+	siteFlags: {},
+	maskedfiles: [],
+	allowedwebsockets: [],
+};
 
 // bundled build will have the wasm binary inlined as a base64 string
 if (REWRITERWASM) {
