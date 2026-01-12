3dc1ac66a5c7ca2434003f3022403ce2879fe8ff
velzie <velzie@velzie.rip>
2026-01-12 09:17:08 -0500

    [scramjet/runway] add basic eval tests

---
+++ packages/runway/src/tests/eval.ts
@@ -0,0 +1,60 @@
+import { basicTest } from "../testcommon.ts";
+
+export default [
+	basicTest({
+		name: "eval-direct-sanity",
+		autoPass: false,
+		js: `
+    	eval("pass()");
+    `,
+	}),
+	basicTest({
+		name: "eval-indirect-sanity",
+		autoPass: false,
+		js: `
+	    (0,eval)("pass()");
+	  `,
+	}),
+	basicTest({
+		name: "eval-direct-rewritten",
+		js: `
+    	eval("checkglobal(top)");
+    `,
+	}),
+	basicTest({
+		name: "eval-indirect-rewritten",
+		js: `
+		  (0,eval)("checkglobal(top)");
+		`,
+	}),
+	basicTest({
+		name: "eval-direct-is-direct",
+		js: `
+			window.local = 231;
+		  {
+				let local = 514;
+				eval("assertEqual(local, 514, 'direct eval should have local scope');");
+				(0,eval)("assertEqual(local, 231, 'indirect eval should have global scope');");
+			}
+		`,
+	}),
+	basicTest({
+		name: "eval-strict",
+		js: `
+			  function testStrictEval() {
+					"use strict";
+					eval("assert((function(){return !this;})() === true, 'strict function should be strict')")
+				}
+				function testSloppyEval() {
+					eval("assert((function(){return this;})() === window, 'sloppy function should be sloppy')")
+				}
+				function testIndirectEval() {
+					"use strict";
+					(0,eval)("assert((function(){return this;})() === window, 'indirect eval should be sloppy even in strict context')")
+				}
+				testStrictEval();
+				testSloppyEval();
+				testIndirectEval();
+			`,
+	}),
+];
