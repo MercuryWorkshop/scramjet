4669240b9b5b8bc5a9653dafe1da2864276a4c9c
percs <me@percs.dev>
2026-04-26 23:16:07 -0500

    add warning to controller about final version

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,2 +1,5 @@
 export * from "./frame";
 export * from "./controller";
+console.warn(
+	"you are using the last version of scramjet v1, if possible, please upgrade to v2 for better performance and more features"
+);
