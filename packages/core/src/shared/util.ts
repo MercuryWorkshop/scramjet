c936a427180bb9cd2340416942a6a462005fafc8
Wyatt Stanke <47758296+Wyatt-Stanke@users.noreply.github.com>
2026-05-02 19:17:09 -0400

    [scramjet/core] use native Uint8Array.toBase64 if present (#155)

--- packages/core/src/shared/util.ts
+++ packages/core/src/shared/util.ts
@@ -1,8 +1,26 @@
 import {
+	Array_from,
 	btoa,
 	TextEncoder_encode,
 	String_fromCharCode,
+	String_fromCodePoint,
 } from "@/shared/snapshot";
+
+function bytesToBase64Fallback(bytes: Uint8Array): string {
+	const binString = Array_from(bytes, (byte) =>
+		String_fromCodePoint(byte)
+	).join("");
+
+	return btoa(binString);
+}
+
+const bytesToBase64Native: ((this: Uint8Array) => string) | undefined =
+	(Uint8Array.prototype as any).toBase64;
+
+export const bytesToBase64: (bytes: Uint8Array) => string =
+	typeof bytesToBase64Native === "function"
+		? (bytes) => bytesToBase64Native.call(bytes)
+		: bytesToBase64Fallback;
 
 export function base64Encode(text: string) {
 	return btoa(
