ec5280ed3fa5d9bf1c3396f3afb273a54d22071a
velzie <velzie@velzie.rip>
2026-05-02 17:38:40 -0400

    [scramjet/core] delete Cache proxy

--- packages/core/src/client/shared/caches.ts
+++ packages/core/src/client/shared/caches.ts
@@ -27,56 +27,5 @@ export default function (client: ScramjetClient, _self: Self) {
 		},
 	});
 
-	client.Proxy("Cache.prototype.add", {
-		apply(ctx) {
-			const url = String(ctx.args[0]);
-			ctx.args[0] = client.rewriteUrl(url);
-		},
-	});
-
-	client.Proxy("Cache.prototype.addAll", {
-		apply(ctx) {
-			const requests = [...ctx.args[0]];
-			for (let i = 0; i < requests.length; i++) {
-				const url = String(requests[i]);
-				requests[i] = client.rewriteUrl(url);
-			}
-			ctx.args[0] = requests;
-		},
-	});
-
-	client.Proxy("Cache.prototype.put", {
-		apply(ctx) {
-			const url = String(ctx.args[0]);
-			ctx.args[0] = client.rewriteUrl(url);
-		},
-	});
-
-	client.Proxy("Cache.prototype.match", {
-		apply(ctx) {
-			const url = String(ctx.args[0]);
-			ctx.args[0] = client.rewriteUrl(url);
-		},
-	});
-
-	client.Proxy("Cache.prototype.matchAll", {
-		apply(ctx) {
-			const url = String(ctx.args[0]);
-			ctx.args[0] = client.rewriteUrl(url);
-		},
-	});
-
-	client.Proxy("Cache.prototype.keys", {
-		apply(ctx) {
-			const url = String(ctx.args[0]);
-			ctx.args[0] = client.rewriteUrl(url);
-		},
-	});
-
-	client.Proxy("Cache.prototype.delete", {
-		apply(ctx) {
-			const url = String(ctx.args[0]);
-			ctx.args[0] = client.rewriteUrl(url);
-		},
-	});
+	// TODO - check if this might leak things if Response from fetch is passed in and the url isn't properly being unrewritten
 }
