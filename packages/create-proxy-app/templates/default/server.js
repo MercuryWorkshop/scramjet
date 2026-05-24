64137c2594d41b628381ae3eb26c9e57fc5616a5
velzie <velzie@velzie.rip>
2026-05-24 10:55:34 -0400

    bump versions

--- packages/create-proxy-app/templates/default/server.js
+++ packages/create-proxy-app/templates/default/server.js
@@ -5,12 +5,15 @@ import express from "express";
 const { routeRequest, routeUpgrade } = await bootstrap();
 
 const app = express();
+app.use((req, res, next) => {
+	if (routeRequest(req, res)) return;
+	next();
+});
 app.use(express.static("public"));
 
 const server = http.createServer(app);
 
 server.on("upgrade", routeUpgrade);
-server.on("request", routeRequest);
 
 server.listen(3030, () => {
 	console.log("Server is running on port 3030");
