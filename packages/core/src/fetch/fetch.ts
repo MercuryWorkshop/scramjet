485c623cd5e9db21c7ded8bf4cdc54621f7d5607
velzie <velzie@velzie.rip>
2026-05-24 10:30:27 -0400

    ci: allow tagged publish

--- packages/core/src/fetch/fetch.ts
+++ packages/core/src/fetch/fetch.ts
@@ -274,8 +274,8 @@ async function handleBlobOrDataUrlFetch(
 		headers.set("Cross-Origin-Opener-Policy", "same-origin");
 		headers.set("Cross-Origin-Embedder-Policy", "require-corp");
 	}
-	
-	if (parsed.isFakeDataURL) URL.revokeObjectURL(dataUrl)
+
+	if (parsed.isFakeDataURL) URL.revokeObjectURL(dataUrl);
 
 	return {
 		body,
