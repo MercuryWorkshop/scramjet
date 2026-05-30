b32b21ed6c64489142be67e54b4d6040015bedeb
velzie <velzie@velzie.rip>
2026-05-29 21:48:36 -0400

    [core] make cross-origin pushstate fail

--- packages/core/src/client/dom/history.ts
+++ packages/core/src/client/dom/history.ts
@@ -1,13 +1,24 @@
 import { ScramjetClient } from "@client/index";
 import { Tap } from "@/Tap";
+import { String, _URL } from "@/shared/snapshot";
 
 export default function (client: ScramjetClient, _self: Self) {
 	client.Proxy(
 		["History.prototype.pushState", "History.prototype.replaceState"],
 		{
 			apply(ctx) {
-				if (ctx.args[2] || ctx.args[2] === "")
-					ctx.args[2] = client.rewriteUrl(ctx.args[2]);
+				const url = String(ctx.args[2]);
+
+				if (_URL.canParse(url)) {
+					const parsedUrl = new _URL(url);
+					if (parsedUrl.origin !== client.url.origin) {
+						// TODO: we want to emulate the proper security error here. right now this will leak the origin in the error message
+						return;
+					}
+				}
+
+				if (url || url === "")
+					ctx.args[2] = client.rewriteUrl(url);
 				ctx.call();
 				Tap.dispatch(
 					client.hooks.lifecycle.navigate,
