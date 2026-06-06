c15e2ee7bd3599f42013da9d4237f202df47c84f
velzie <velzie@velzie.rip>
2026-06-06 10:31:49 -0400

    core,chrome: fix displaced history.pushState calls using the wrong client for logic

--- packages/core/src/client/dom/history.ts
+++ packages/core/src/client/dom/history.ts
@@ -7,25 +7,26 @@ export default function (client: ScramjetClient, _self: Self) {
 		["History.prototype.pushState", "History.prototype.replaceState"],
 		{
 			apply(ctx) {
+				const relevantclient = client.box.histories.get(ctx.this);
 				const url = String(ctx.args[2]);
 
 				if (_URL.canParse(url)) {
 					const parsedUrl = new _URL(url);
-					if (parsedUrl.origin !== client.url.origin) {
+					if (parsedUrl.origin !== relevantclient.url.origin) {
 						// TODO: we want to emulate the proper security error here. right now this will leak the origin in the error message
 						return;
 					}
 				}
 
-				if (url || url === "") ctx.args[2] = client.rewriteUrl(url);
+				if (url || url === "") ctx.args[2] = relevantclient.rewriteUrl(url);
 				ctx.call();
 				Tap.dispatch(
-					client.hooks.lifecycle.navigate,
+					relevantclient.hooks.lifecycle.navigate,
 					{
 						type: "history",
 					},
 					{
-						url: client.url.href,
+						url: relevantclient.url.href,
 					}
 				);
 			},
